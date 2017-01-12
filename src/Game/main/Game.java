9adf17b0244e494bae2dca7ae403d05ae2a68784
CosmeT17 <cosmetejeda13@gmail.com>
2017-01-12 14:45:57 -0600

    Test Change 2

--- src/Game/main/Game.java
+++ src/Game/main/Game.java
@@ -40,7 +40,7 @@ public class Game extends Canvas implements Runnable {
 		
 		this.addKeyListener(new KeyInput(handler));
 		this.addMouseListener(menu);
-		new Window(WIDTH, HEIGHT, "Kyma 1.0.0", this);
+		new Window(WIDTH, HEIGHT, "Kyma 1.1.0", this);
 		
 		hud = new HUD();
 		spawner = new Spawn(handler, hud);
